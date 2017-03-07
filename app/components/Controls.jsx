e28080ba92114b6be918d80aa291a9f45355c823
Evan Albert <evan@cyberdust.com>
2017-03-07 09:27:38 -0800

    countdown timer function and styling

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
+const React = require('react');
 
-class Controls extends React.Component {
-  render () {
+var Controls = React.createClass({
+  propTypes: {
+    countdownStatus: React.PropTypes.string.isRequired,
+    onStatusChange: React.PropTypes.func.isRequired
+  },
+  onStatusChange: function (newStatus) {
+    return () => {
+      this.props.onStatusChange(newStatus);
+    }
+  },
+  render: function () {
     var {countdownStatus} = this.props;
 
     var renderStartStop = () => {
         if (countdownStatus === 'started') {
-          return <button className="button secondary">Pause</button>
+          return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
         } else if (countdownStatus === 'paused') {
-          return <button className="button primary">Start</button>
+          return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
         }
     }
 
     return (
       <div className="controls">
         {renderStartStop()}
-        <button className="button alert hollow">
+        <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>
           Clear
         </button>
       </div>
     )
   }
-}
-
-Controls.propTypes = {
-  countdownStatus: React.PropTypes.string.isRequired
-}
-
+});
 
 module.exports = Controls;
